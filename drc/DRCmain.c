d9ed40feec390b15f7606c846e66bdf66107d408
Daniel Hwang <daniel@dangerdevices.com>
2024-01-01 20:04:59 -0500

    style drc: add print to output as lambda or microns

--- drc/DRCmain.c
+++ drc/DRCmain.c
@@ -184,7 +184,7 @@ drcSubstitute (cptr)
     DRCCookie * cptr;  		/* Design rule violated */
 {
     static char *why_out = NULL;
-    char *whyptr, *sptr, *wptr;
+    char *whyptr, *sptr, *wptr, *unit;
     int subscnt = 0, whylen;
     float oscale, value;
     extern float CIFGetOutputScale();
@@ -203,10 +203,20 @@ drcSubstitute (cptr)
     why_out = (char *)mallocMagic(whylen * sizeof(char));
     strcpy(why_out, whyptr);
 
-    if (cptr->drcc_flags & DRC_CIFRULE)
-	oscale = CIFGetScale(100);	/* 100 = microns to centimicrons */
+    if (DRCPrintConvert)
+    {
+	unit = "um";
+	if (cptr->drcc_flags & DRC_CIFRULE)
+	    oscale = CIFGetScale(100);	/* 100 = microns to centimicrons */
+	else
+	    oscale = CIFGetOutputScale(1000);   /* 1000 for conversion to um */
+    }
     else
-	oscale = CIFGetOutputScale(1000);   /* 1000 for conversion to um */
+    {
+	unit = "lambda";
+	oscale = 1;
+    }
+
     wptr = why_out;
 
     while ((sptr = strchr(whyptr, '%')) != NULL)
@@ -218,21 +228,21 @@ drcSubstitute (cptr)
 	switch (*(sptr + 1))
 	{
 	    case 'd':
-		/* Replace with "dist" value in microns */
+		/* Replace with "dist" value in microns or lambda */
 		value = (float)cptr->drcc_dist * oscale;
-		snprintf(wptr, 20, "%01.3gum", value);
+		snprintf(wptr, 20, "%01.3g%s", value, unit);
 		wptr += strlen(wptr);
 		break;
 	    case 'c':
-		/* Replace with "cdist" value in microns */
+		/* Replace with "cdist" value in microns or lambda */
 		value = (float)cptr->drcc_cdist * oscale;
-		snprintf(wptr, 20, "%01.3gum", value);
+		snprintf(wptr, 20, "%01.3g%s", value, unit);
 		wptr += strlen(wptr);
 		break;
 	    case 'a':
-		/* Replace with "cdist" value in microns squared */
+		/* Replace with "cdist" value in microns or lambda squared */
 		value = (float)cptr->drcc_cdist * oscale * oscale;
-		snprintf(wptr, 20, "%01.4gum^2", value);
+		snprintf(wptr, 20, "%01.4g%s^2", value, unit);
 		wptr += strlen(wptr);
 		break;
 	    default:
